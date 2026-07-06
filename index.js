b648455cefdc292ab578c89d648a2c9159aec1a5
ehabhanyharp-source <ehab.hany.harp@gmail.com>
2026-07-06 16:29:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-const { Telegraf } = require('telegraf');
+const TelegramBot = require("node-telegram-bot-api");
 
-// التوكن الخاص بك
-const bot = new Telegraf('8892358205:AAGH4t_I2Mk7eBANZ0_z8fTkmKj4xqxTs6c');
+const token = "8892358205:AAGH4t_I2Mk7eBANZ0_z8fTkmKj4xqxTs6c";
 
-// معالجة الرسائل
-bot.start((ctx) => ctx.reply('البوت يعمل الآن من ملف index.js!'));
-bot.on('message', (ctx) => ctx.reply('وصلت رسالتك.'));
+const bot = new TelegramBot(token, { polling: true });
 
-// الدالة الأساسية لاستقبال التحديثات من تليجرام
-module.exports = async (req, res) => {
-    try {
-        await bot.handleUpdate(req.body);
-        return res.status(200).send('OK');
-    } catch (err) {
-        return res.status(500).send('Error');
-    }
-};
+console.log("✅ Bot is running...");
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🎉 البوت شغال بنجاح!");
+});
