d8bcbf46cb1100a351d9392fb9929859bbe3e0ce
ehabhanyharp-source <ehab.hany.harp@gmail.com>
2026-07-06 16:14:28 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,22 +1,15 @@
 const { Telegraf } = require('telegraf');
-
-// التوكن الجديد الخاص بك موضوع مباشرة هنا
-const bot = new Telegraf('8892358205:AAGH4t_I2Mk7eBANZ0_z8fTkmKj4xqxTs6c');
-
-bot.start((ctx) => {
-    ctx.reply('مرحباً بك في بوت Ustern الجديد! أنا جاهز للخدمة.');
-});
-
-bot.on('text', (ctx) => {
-    ctx.reply('لقد استلمت رسالتك بنجاح.');
-});
+const bot = new Telegraf('8840523796:AAEAFM5-MBd5Eq2DFBv3WQPjTjXT5V-XOOI');
 
 module.exports = async (req, res) => {
     try {
-        await bot.handleUpdate(req.body);
-        return res.status(200).send('OK');
+        if (req.method === 'POST') {
+            await bot.handleUpdate(req.body);
+            return res.status(200).send('OK');
+        }
+        return res.status(200).send('Bot is running!');
     } catch (err) {
-        console.error('حدث خطأ:', err);
+        console.error(err);
         return res.status(500).send('Error');
     }
 };
