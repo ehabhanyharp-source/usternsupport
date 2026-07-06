1c1289630e1a750694501bf8d4b3f4af0ee2ade3
ehabhanyharp-source <ehab.hany.harp@gmail.com>
2026-07-06 15:35:25 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,22 @@
-{
-  "rewrites": [
-    { "source": "/api/webhook", "destination": "/api/webhook.js" }
-  ]
-}
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('8840523796:AAEAFM5-MBd5Eq2DFBv3WQPjTjXT5V-XOOI');
+
+// إعداد الأوامر هنا
+bot.start((ctx) => ctx.reply('يعمل بنجاح!'));
+
+module.exports = async (req, res) => {
+    try {
+        // استلام البيانات من تليجرام
+        const update = req.body;
+        
+        // محاولة المعالجة
+        await bot.handleUpdate(update);
+        
+        // الرد على تليجرام أننا استلمنا الرسالة
+        return res.status(200).send('OK');
+    } catch (err) {
+        // إذا حدث خطأ، سنعرفه في الـ Logs
+        console.error("خطأ في البوت:", err);
+        return res.status(500).send('Internal Server Error');
+    }
+};
