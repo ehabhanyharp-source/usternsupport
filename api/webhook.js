180d4cf7109afc8c561eb2ac8b478279b711281c
ehabhanyharp-source <ehab.hany.harp@gmail.com>
2026-07-06 16:00:24 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,13 +1,17 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const bot = new Telegraf('8840523796:AAEAFM5-MBd5Eq2DFBv3WQPjTjXT5V-XOOI');
 
-bot.start((ctx) => ctx.reply('البوت يعمل الآن!'));
+bot.start((ctx) => {
+    ctx.reply('أهلاً بك في دعم Ustern! كيف يمكنني مساعدتك اليوم؟', Markup.inlineKeyboard([
+        [Markup.button.callback('تتبع الطلب', 'track_order')],
+        [Markup.button.callback('تواصل مع الدعم', 'contact_support')]
+    ]));
+});
+
+bot.action('track_order', (ctx) => ctx.reply('من فضلك أرسل رقم الطلب الخاص بك.'));
+bot.action('contact_support', (ctx) => ctx.reply('سيتم تحويلك لموظف الدعم الآن.'));
 
 module.exports = async (req, res) => {
-    try {
-        await bot.handleUpdate(req.body);
-        return res.status(200).send('OK');
-    } catch (err) {
-        return res.status(500).send('Error');
-    }
+    await bot.handleUpdate(req.body);
+    return res.status(200).send('OK');
 };
